593a11c6ce91786b5ac60bcf907d1bfbcb586c7d
David Ajnered <davidajnered@gmail.com>
2014-09-07 14:20:05 +0200

    Offset and limit improvements

--- app/Core.php
+++ app/Core.php
@@ -1,6 +1,10 @@
 <?php
-/*
-*/
+/**
+ * Wordpress Popular Posts API
+ *
+ * Written by David Ajnered
+ * http://davidajnered.com
+ */
 namespace WordpressPopularPostsApi;
 
 class Core extends \WordpressPopularPosts
@@ -11,9 +15,16 @@ class Core extends \WordpressPopularPosts
      * @param array $options
      * @return array $postIds
      */
-    public function getPopularPostIds(array $options = array())
+    public function getPopularPosts(array $options = array())
     {
+        $status = '';
         $options = array_merge($this->defaults, $options);
+
+        // Save requested limit for later
+        $limit = $options['limit'];
+        // Update limit passed to query with the offset.
+        $options['limit'] = $this->getLimit($options);
+        // Query Wordpress Popular Posts
         $results = $this->_query_posts($options);
 
         $postIds = array();
@@ -22,11 +33,27 @@ class Core extends \WordpressPopularPosts
         }
 
         // Return the same number of posts as option['limit']
-        if (count($postIds) < $options['limit']) {
+        if (count($postIds) < $limit && (isset($options['fill_results']) && $options['fill_results'])) {
             $postIds = $this->addExtraPostIds($postIds, $options);
         }
 
         return $postIds;
+    }
+
+    /**
+     * Get query limit based on limit and offset.
+     *
+     * @param array $options
+     * @return string $limit
+     */
+    private function getLimit($options)
+    {
+        $limit = $options['posts_per_page'];
+        if (isset($options['offset']) && is_numeric($options['offset'])) {
+            $limit = $options['offset'] . ',' . $limit;
+        }
+
+        return $limit;
     }
 
     /**
@@ -40,7 +67,7 @@ class Core extends \WordpressPopularPosts
     {
         // Set WP options
         $options['post__not_in'] = $postIds;
-        $options['posts_per_page'] = (int) $options['limit'] - count($postIds);
+        $options['posts_per_page'] = (int) $options['posts_per_page'] - count($postIds);
 
         $posts = get_posts($options);
         foreach ($posts as $post) {
